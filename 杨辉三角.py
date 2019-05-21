d48dff972c7d83de73b2df77805e1fb7112d835f
Wu Lin <1767504797@qq.com>
2019-05-22 00:05:06 +0800

    杨辉三角形-Python生成器

---
+++ 杨辉三角.py
@@ -0,0 +1,27 @@
+import copy
+
+
+def triangles():
+    num = 0
+    while True:
+        if num == 0:
+            num += 1
+            yield [1]
+        elif num == 1:
+            num += 1
+            tmp = [1,1]
+            yield [1,1] 
+        else:
+            length = tmp.__len__() + 1
+            tmp = [1 if i == 0 or i == length - 1 else tmp[i] + tmp[i - 1] for i in range(length)]
+            yield tmp
+
+
+if __name__ == '__main__':
+    n = 0
+    results = []
+    for t in triangles():
+        print(t)
+        n = n + 1
+        if n == 10:
+            break
